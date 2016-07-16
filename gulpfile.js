627d4455ca165698303082a5e8d3ab8e96099fa3
Jason Miller <jason@millerjb.com>
2016-07-15 23:21:46 -0600

    test(coverage): wire in code coverage tools

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,51 @@
 
 const gulp = require('gulp')
   , plugins = require('gulp-load-plugins')()
+  , istanbul = require('gulp-istanbul')
   , yargs = require('yargs')
   , chai = require('chai')
   , path = require('path');
 
-gulp.task('test.lib', function() {
-  global.expect = chai.expect;
-  global.sut = function(sutPath) {
-    return require(path.join(__dirname, sutPath));
-  };
+function runMocha(done) {
+  return function() {
+    global.expect = chai.expect;
+    global.sut = function(sutPath) {
+      return require(path.join(__dirname, sutPath));
+    };
 
-  var mochaOpts = {
-    grep: yargs.argv.grep,
-    reporter: 'spec'
-  };
+    var mochaOpts = {
+      grep: yargs.argv.grep,
+      reporter: 'spec'
+    };
 
-  gulp.src('test/lib/**/*spec.js')
-    .pipe(plugins.mocha(mochaOpts));
+    var stream = gulp.src('test/lib/**/*spec.js')
+      .pipe(plugins.mocha(mochaOpts));
+
+    if (yargs.argv.coverage) {
+      stream.pipe(istanbul.writeReports({
+        reporters: ['text', 'lcov', 'html'],
+        reportOpts: {
+          dir: '.tmp/node/reports/coverage',
+        }
+      }));
+    }
+
+    stream.on('end', done);
+  }
+}
+
+gulp.task('test.node', function(done) {
+  if (yargs.argv.coverage) {
+    gulp.src('lib/**/*.js')
+      .pipe(istanbul({
+        includeUntested: true
+      }))
+      .pipe(istanbul.hookRequire())
+      .on('finish', runMocha(done));
+  } else {
+    runMocha(done)();
+  }
+
 });
 
-gulp.task('default', ['test.lib']);
+gulp.task('default', ['test.node']);
