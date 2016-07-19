a0b42ec979a73b5636e1d51764b0a0805aeaf5bf
Jason Miller <jason@millerjb.com>
2016-07-18 21:47:44 -0600

    feat(chat): add a basic plc_msg / sort responder

--- test/lib/xle/chat-message.model.spec.js
+++ test/lib/xle/chat-message.model.spec.js
@@ -7,26 +7,24 @@ describe('ChatMessage', () => {
   it('should deserialize field types', () => {
     const text = 'SCN,SCT,RCN,RCT,MT,MSGID,PKGUID,TRKNUM,DEST1,DEST2,DEST3,DEST4';
     var msg = ChatMessage.fromText(text);
-    expect(msg.getSenderName()).to.equal('SCN');
-    expect(msg.getSenderType()).to.equal('SCT');
-    expect(msg.getReceiverName()).to.equal('RCN');
-    expect(msg.getReceiverType()).to.equal('RCT');
-    expect(msg.getMessageType()).to.equal('MT');
-    expect(msg.getMessageId()).to.equal('MSGID');
-    expect(msg.getDataFields()).to.deep.equal(['PKGUID','TRKNUM','DEST1','DEST2','DEST3','DEST4']);
+    expect(msg.senderName).to.equal('SCN');
+    expect(msg.senderType).to.equal('SCT');
+    expect(msg.receiverName).to.equal('RCN');
+    expect(msg.receiverType).to.equal('RCT');
+    expect(msg.messageType).to.equal('MT');
+    expect(msg.dataFields).to.deep.equal(['MSGID', 'PKGUID','TRKNUM','DEST1','DEST2','DEST3','DEST4']);
   });
 
   it('should deserialize sample message', () => {
     const text = '10.1.1.2,XLE-0001,10.1.1.3,PFC-0123,SORT,TT–0001–23:59:01:998–12345678901,'
       + 'TR12341234567890A0A0A0A001,1Z0318017912345678,00001,09999,00123,01111';
     var msg = ChatMessage.fromText(text);
-    expect(msg.getSenderName()).to.equal('10.1.1.2');
-    expect(msg.getSenderType()).to.equal('XLE-0001');
-    expect(msg.getReceiverName()).to.equal('10.1.1.3');
-    expect(msg.getReceiverType()).to.equal('PFC-0123');
-    expect(msg.getMessageType()).to.equal('SORT');
-    expect(msg.getMessageId()).to.equal('TT–0001–23:59:01:998–12345678901');
-    expect(msg.getDataFields()).to.deep.equal(['TR12341234567890A0A0A0A001', '1Z0318017912345678', '00001', 
+    expect(msg.senderName).to.equal('10.1.1.2');
+    expect(msg.senderType).to.equal('XLE-0001');
+    expect(msg.receiverName).to.equal('10.1.1.3');
+    expect(msg.receiverType).to.equal('PFC-0123');
+    expect(msg.messageType).to.equal('SORT');
+    expect(msg.dataFields).to.deep.equal(['TT–0001–23:59:01:998–12345678901', 'TR12341234567890A0A0A0A001', '1Z0318017912345678', '00001', 
       '09999', '00123', '01111']);
   });
 
@@ -37,21 +35,21 @@ describe('ChatMessage', () => {
   });
 
   it('should error without insufficient fields', () => {
-    const text = 'field1,field2,field3,field4,field5';
+    const text = 'field1,field2,field3,field4';
     expect(() => {
       ChatMessage.fromText(text);
-    }).to.throw('unable to deserialize message - only 5 fields!');
+    }).to.throw('unable to deserialize message - only 4 field(s)!');
   });
 
   it('should serialize field types', () => {
-    var msg = new ChatMessage('SCN', 'SCT', 'RCN', 'RCT', 'MT', 'MSGID', ['PKGUID', 'TRKNUM', 'DEST1', 'DEST2', 'DEST3', 'DEST4']);
+    var msg = new ChatMessage('SCN', 'SCT', 'RCN', 'RCT', 'MT', ['MSGID', 'PKGUID', 'TRKNUM', 'DEST1', 'DEST2', 'DEST3', 'DEST4']);
     expect(msg.toText()).to.equal('SCN,SCT,RCN,RCT,MT,MSGID,PKGUID,TRKNUM,DEST1,DEST2,DEST3,DEST4');
   });
 
 
   it('should serialize field types', () => {
-    var msg = new ChatMessage('10.1.1.2', 'XLE-0001', '10.1.1.3', 'PFC-0123', 'SORT', 'TT–0001–23:59:01:998–12345678901', 
-      ['TR12341234567890A0A0A0A001', '1Z0318017912345678', '00001', '09999', '00123', '01111']);
+    var msg = new ChatMessage('10.1.1.2', 'XLE-0001', '10.1.1.3', 'PFC-0123', 'SORT', ['TT–0001–23:59:01:998–12345678901', 
+      'TR12341234567890A0A0A0A001', '1Z0318017912345678', '00001', '09999', '00123', '01111']);
     expect(msg.toText()).to.equal('10.1.1.2,XLE-0001,10.1.1.3,PFC-0123,SORT,TT–0001–23:59:01:998–12345678901,'
       + 'TR12341234567890A0A0A0A001,1Z0318017912345678,00001,09999,00123,01111');
   });
