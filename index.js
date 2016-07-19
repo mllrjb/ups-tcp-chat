12cc4427444d4bbc89d6472ad5f2c4f91f286904
Jason Miller <jason@millerjb.com>
2016-07-18 22:11:57 -0600

    feat(config): add configuration file

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
-const winston = require('winston')
+'use strict';
+ 
+const path = require('path')
+  , fs = require('fs')
+  , winston = require('winston')
+  , nconf = require('nconf')
   , TcpServer = require('./lib/tcp/tcp-server.js')
   , ChatController = require('./lib/xle/chat.controller');
 
-winston.level = 'debug';
 
-const TCP_CHAT_PORT = 9001;
+const configPath = path.join(__dirname, 'config.yml');
 
-const tcpServer = new TcpServer({
-  port: TCP_CHAT_PORT
+nconf.env({separator: '__'}).argv();
+nconf.formats.yaml = require('nconf-yaml');
+
+try {
+  if (fs.statSync(configPath).isFile()) {
+    nconf.file({ file: configPath, format: nconf.formats.yaml });
+  }
+} catch(err) {
+  // ignore
+}
+
+
+nconf.defaults({
+  logger: {
+    level: 'info'
+  },
+  server: {
+    port: 9000
+  }
 });
 
-tcpServer.on('connected', () => {
-  console.log('TCP server listening at localhost:' + TCP_CHAT_PORT);
+winston.level = nconf.get('logger:level');
+
+const tcpServer = new TcpServer({
+  port: nconf.get('server:port')
 });
 
 const chatCtrl = new ChatController(tcpServer); 
