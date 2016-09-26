7f801178b88e4df4d5836db02c49f26dea866e71
Jason Miller <jason@millerjb.com>
2016-09-25 19:59:35 -0600

    fix(dist): upgrade nodejs to 6.x

--- dist.sh
+++ dist.sh
@@ -8,7 +8,7 @@ rm -rf node_modules
 npm i --production
 
 if [ ! -f 'node.exe' ]; then
-  wget https://nodejs.org/dist/v4.4.7/win-x64/node.exe
+  wget https://nodejs.org/dist/v6.6.0/win-x64/node.exe
 fi
 
 # zip everything up
